37c8f45d991b775fbee58f315dd82dff44d39c19
Herman Liu <hermanliu95@gmail.com>
2021-03-17 07:49:23 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Testimonial Component
+
+This is a challenge from Front End Mentor.
+
+View the challenge here: https://www.frontendmentor.io/challenges/coding-bootcamp-testimonials-slider-4FNyLA8JL
+
+
+
+## Brief
+Users should be able to:
+* View the optimal layout for the site depending on their device's screen size
+* Navigate the slider using either their mouse/trackpad or keyboard 
+
---
+++ assets/app.js
@@ -0,0 +1,61 @@
+// const prevBtn = document.getElementById('prev-btn')
+// const nextBtn = document.getElementById('next-btn')
+
+let userImage = document.getElementById('user-image')
+let userTestimonial = document.getElementById('testimonial')
+let userName = document.getElementById('name')
+let userRole = document.getElementById('role')
+let btns = document.querySelectorAll('.btns')
+
+let position = 0
+
+let tanya = {
+    img: './assets/images/image-tanya.jpg',
+    testimonial: `
+    “ I’ve been interested in coding for a while but never taken the jump, until now. 
+    I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+    excited about the future. ”
+    `,
+    name: 'Tanya Sinclair',
+    role: 'UX Engineer'
+}
+
+let john = {
+    img: './assets/images/image-john.jpg',
+    testimonial: `
+    “ If you want to lay the best foundation possible I’d recommend taking this course. 
+    The depth the instructors go into is incredible. I now feel so confident about 
+    starting up as a professional developer. ”
+    `,
+    name: 'John Tarkpor',
+    role: 'Junior Front-end Developer'
+}
+
+for(let i = 0; i < btns.length; i ++) {
+    btns[i].addEventListener('click', () => {
+        position++
+        // console.log(position)
+        switch(position) {
+            case 1: 
+                userImage.src = john.img
+                userTestimonial.textContent = john.testimonial
+                userName.textContent = john.name
+                userRole.textContent = john.role
+                isTanya = false
+                isJohn = true   
+                break
+            case 2:
+                userImage.src = tanya.img
+                userTestimonial.textContent = tanya.testimonial
+                userName.textContent = tanya.name
+                userRole.textContent = tanya.role
+                isJohn = false
+                isTanya = true
+                break
+        }
+        if(position == 2) {
+            position = 0
+        }
+    })
+}
+
